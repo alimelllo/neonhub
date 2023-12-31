8b5cef16017e0db155edd2a710b3d55fc7c9bd8b
alimelllo <ali.melllo@yahoo.com>
2024-01-01 01:12:05 +0330

    new ui testing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,48 +8,62 @@ import SlideShow from '@/Components/GeneralComponents/SlideShow';
 export default function Home() {
 
   return (
-    <div className='overflow-x-hidden flex flex-col h-screen bg-[#000000] dark:bg-[#000000]'>
-      <Header />
-      <main className={`flex flex-col justify-center h-screen`}>
+    // <div className='overflow-x-hidden flex flex-col h-screen bg-[#000000] dark:bg-[#000000]'>
+    //   <Header />
+    //   <main className={`flex flex-col justify-center h-screen`}>
 
-        {/* ================== */}
-        {/* FIRST MAIN SECTION */}
+    //     {/* ================== */}
+    //     {/* FIRST MAIN SECTION */}
 
 
-        <div className='flex flex-row md:flex-col-reverse w-full'>
+    //     <div className='flex flex-row md:flex-col-reverse w-full'>
 
-          <div className='w-6/12 md:w-full md:h-[55%] md:mx-auto relative contrast-[1.1]'>
-            <Image layout={'responsive'} alt={'Neon Hub'} src={wall} />
-            <div className="neon-text text-[5rem] md:text-[2.5rem] mt-[5rem] md:mt-0 md:mb-[10rem] font-[Vibur]">
-              <b>N<span>EON</span> H<span>U</span>B</b>
-              <div className='w-full mt-10 md:mt-2 flex justify-center'>
-                <p className='text-[#ebebeb] font-[Vazir] text-center w-5/12 md:w-full text-[1.7rem] md:text-[1rem] flex flex-wrap md:justify-center'>
-                  ثبت سفارش طرح های دلخواه چراغ های نئون
-                </p>
-              </div>
-              <div className='w-full md:bg-[black] md:p-[2rem] flex flex-row md:flex-col mt-[5rem] md:mt-[0.5rem] justify-center'>
-                <button className='text-[white] mx-2 md:mx-auto bg-[#a34062] hover:bg-[#5670cd] transition-all duration-100 font-[Vazir] p-2 rounded-[5px] text-center w-3/12 md:w-11/12 text-[1.25rem]'>
-                  محصولات
-                </button>
-                <button className='text-[white] md:hidden mx-2 bg-[#344995] hover:bg-[#953859] transition-all duration-100 font-[Vazir] p-2 rounded-[5px] text-center w-3/12 md:w-9/12 text-[1.25rem]'>
-                  ثبت سفارش
-                </button>
-              </div>
-            </div>
-          </div>
+    //       <div className='w-6/12 md:w-full md:h-[55%] md:mx-auto relative contrast-[1.1]'>
+    //         <Image layout={'responsive'} alt={'Neon Hub'} src={wall} />
+    //         <div className="neon-text text-[5rem] md:text-[2.5rem] mt-[5rem] md:mt-0 md:mb-[10rem] font-[Vibur]">
+    //           <b>N<span>EON</span> H<span>U</span>B</b>
+    //           <div className='w-full mt-10 md:mt-2 flex justify-center'>
+    //             <p className='text-[#ebebeb] font-[Vazir] text-center w-5/12 md:w-full text-[1.7rem] md:text-[1rem] flex flex-wrap md:justify-center'>
+    //               ثبت سفارش طرح های دلخواه چراغ های نئون
+    //             </p>
+    //           </div>
+    //           <div className='w-full md:bg-[black] md:p-[2rem] flex flex-row md:flex-col mt-[5rem] md:mt-[0.5rem] justify-center'>
+    //             <button className='text-[white] mx-2 md:mx-auto bg-[#a34062] hover:bg-[#5670cd] transition-all duration-100 font-[Vazir] p-2 rounded-[5px] text-center w-3/12 md:w-11/12 text-[1.25rem]'>
+    //               محصولات
+    //             </button>
+    //             <button className='text-[white] md:hidden mx-2 bg-[#344995] hover:bg-[#953859] transition-all duration-100 font-[Vazir] p-2 rounded-[5px] text-center w-3/12 md:w-9/12 text-[1.25rem]'>
+    //               ثبت سفارش
+    //             </button>
+    //           </div>
+    //         </div>
+    //       </div>
 
-          <div className='w-6/12 md:w-full flex justify-center pt-[3rem] md:pt-[15rem] items-center h-[100%] md:h-[45%]'>
-            <SlideShow data={[]} />
-          </div>
+    //       <div className='w-6/12 md:w-full flex justify-center pt-[3rem] md:pt-[15rem] items-center h-[100%] md:h-[45%]'>
+    //         <SlideShow data={[]} />
+    //       </div>
+    //     </div>
+
+
+    //     {/* FIRST MAIN SECTION */}
+    //     {/* ================== */}
+
+
+    //   </main>
+    // </div>
+    <div className='h-screen relative'>
+      <div className="container ">
+        <div className="sky">
+          <div className="stars"></div>
+          <div className="stars1"></div>
+          <div className="stars2"></div>
+          <div className="shooting-stars"></div>
         </div>
-
-
-        {/* FIRST MAIN SECTION */}
-        {/* ================== */}
-
-
-      </main>
+      </div>
+      <div className='absolute top-16 bottom-16 right-16 left-16 blure bg-gradient-to-tr from-[#02010874] to-[#12093074]'>
+      <div className="neon-text text-[5rem] md:text-[2.5rem] font-[Vibur]">
+               <b>N<span>EON</span> H<span>U</span>B</b>
     </div>
-
+      </div>
+    </div>
   )
 }
