d36825d6b38099d32ddcbbc56b7829f51858b9f6
Mosadeq <sani.mosadegh@gmail.com>
2024-01-01 17:20:17 +0330

    initial portfolio ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -50,20 +50,117 @@ export default function Home() {
 
     //   </main>
     // </div>
-    <div className='h-screen relative'>
-      <div className="container ">
-        <div className="sky">
-          <div className="stars"></div>
-          <div className="stars1"></div>
-          <div className="stars2"></div>
-          <div className="shooting-stars"></div>
+    <>
+      <div className='h-screen relative bg-[#160430]'>
+
+        <div className="container">
+
+
+          <div className="sky">
+            <div className="stars"></div>
+            <div className="stars1"></div>
+            <div className="stars2"></div>
+            <div className="shooting-stars"></div>
+            <div className="shooting-stars-2"></div>
+            <div className="shooting-stars-3"></div>
+          </div>
+        </div>
+
+
+        <div className="content ">
+          <div className="planet ">
+            <div className="ring"></div>
+            <div className="cover-ring"></div>
+            <div className="spots">
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+          </div>
+        </div>
+
+        <div className='absolute font-[Orbit] z-[10] rounded-[5px] flex top-28 bottom-28 right-40 left-40 blure bg-gradient-to-tr from-[#02010874] to-[#12093074]'>
+
+          <div className='flex flex-row w-full justify-between pt-10'>
+            <div className='w-6/12 pl-[5rem]'>
+              <p className='text-[2.5rem]'>Ali Maleki</p>
+              <p className='text-[1.25rem] tracking-wider text-[#a6a6a6cf]'>Front End Developer</p>
+              <p className='text-[1rem] tracking-wider mt-[4rem] flex flex-wrap text-justify text-[#cecece]'>
+                I am 24 years old focusing on javascript & Front End development.
+                I've got 3 years of experience collaborating with team work on-site and remotely.
+                I like creating mangeable complex ui and large scale applications using React And its combination with TypeScript.
+                I also have a basic knowledge of back end ( Node js , Express ).
+              </p>
+            </div>
+
+            <div className='w-6/12 flex justify-center items-center'>
+               <div className='bg-gradient-to-br from-[#d52c6a6f] rounded-lg boxShadowLight to-[#3f3fb755] h-[90%] mb-10 w-6/12'>
+
+               </div>
+            </div>
+
+          </div>
+
+          <div className="space">
+            <div className="ship">
+              <div className="ship-rotate">
+                <div className="pod"></div>
+                <div className="fuselage"></div>
+              </div>
+            </div>
+            {/* <div className="ship-shadow"></div> */}
+            <div className="mars">
+              {/* <div className="tentacle"></div> */}
+              <div className="flag">
+                {/* <div className="small-tentacle"></div> */}
+              </div>
+              <div className="planet ">
+                <div className="surface"></div>
+                <div className="crater1"></div>
+                <div className="crater2"></div>
+                <div className="crater3"></div>
+              </div>
+            </div>
+          </div>
+
+
+
+          <div className="planet_wraper planet_shadow">
+            <div className="planet-green">
+              <div className="crater"></div>
+              <div className="crater"></div>
+              <div className="crater"></div>
+              <div className="crater"></div>
+              <div className="crater"></div>
+            </div>
+          </div>
+
+
+
+
+
+
+
+
+
+
         </div>
       </div>
-      <div className='absolute top-16 bottom-16 right-16 left-16 blure bg-gradient-to-tr from-[#02010874] to-[#12093074]'>
-      <div className="neon-text text-[5rem] md:text-[2.5rem] font-[Vibur]">
-               <b>N<span>EON</span> H<span>U</span>B</b>
-    </div>
-      </div>
-    </div>
+
+
+
+
+
+
+
+
+
+
+
+    </>
   )
 }
